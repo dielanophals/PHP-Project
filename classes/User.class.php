d703ea6c3af18b796154da57f4db965c64114786
haege <haegeman.irene@hotmail.com>
2019-04-06 11:04:25 +0200

    Add setters

--- classes/User.class.php
+++ classes/User.class.php
@@ -5,4 +5,29 @@
         private $lastname;
         private $username;
         private $password;
+
+        public function setEmail($email){
+            $this->email = $email;
+            return $this;
+        }
+
+        public function setFirstname($firstname){
+            $this->firstname = $firstname;
+            return $this;
+        }
+
+        public function setLastname($lastname){
+            $this->lastname = $lastname;
+            return $this;
+        }
+
+        public function setUsername($username){
+            $this->username = $username;
+            return $this;
+        }
+
+        public function setPassword($password){
+            $this->password = $password;
+            return $this;
+        }
     }
