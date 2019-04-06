281f064246bf014f77bbd2bf2e36de22066ea70b
haege <haegeman.irene@hotmail.com>
2019-04-06 10:52:00 +0200

    Add form

--- login.php
+++ login.php
@@ -11,6 +11,16 @@
     <title>InstaPet - Login</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <h2>Sign in</h2>
+        <label for="email">Email</label>
+        <input type="text" name="email">
+
+        <label for="password">Password</label>
+        <input type="password" name="password">
+
+        <input type="submit" value="Sign in">
+        <input type="checkbox"><label for="rememberMe">Remember me</label>
+    </form>
 </body>
 </html>
