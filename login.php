05b600ddc007b08662659f32d7abf11b3de7407d
haege <haegeman.irene@hotmail.com>
2019-04-06 15:29:51 +0200

    Add breaks after labels

--- login.php
+++ login.php
@@ -35,12 +35,12 @@
             </div>
         <?php endif; ?>
         <label for="email">Email</label>
-        <input type="text" name="email">
+        <input type="text" name="email"><br>
 
         <label for="password">Password</label>
-        <input type="password" name="password">
+        <input type="password" name="password"><br>
 
-        <input type="submit" value="Sign in">
+        <input type="submit" value="Sign in"><br>
         <input type="checkbox"><label for="rememberMe">Remember me</label>
     </form>
 </body>
