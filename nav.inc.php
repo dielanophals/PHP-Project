75004662b8a4c3bff5ac55089fa076735f9b0737
haege <haegeman.irene@hotmail.com>
2019-04-06 14:32:28 +0200

    Create nav.inc.php

---
+++ nav.inc.php
@@ -0,0 +1,10 @@
+<nav>
+    <a href="index.php" class="logo">Instapet</a>
+    <a href="index.php">Home</a>
+    
+    <form action="" method="get">
+      <input type="text" name="search">
+    </form>
+    
+    <a href="logout.php" class="navbar__logout">logout</a>
+</nav>
