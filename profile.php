80679f107fd0c93fc43bbd147ab656c4b365374e
dielanophals <43468300+dielanophals@users.noreply.github.com>
2019-04-09 01:18:39 +0200

    Added navigation to profile

--- profile.php
+++ profile.php
@@ -10,9 +10,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel = "stylesheet" type = "text/css" href = "css/reset.css"/>
+    <link rel = "stylesheet" type = "text/css" href = "css/style.css"/>
     <title>InstaPet - Profile</title>
 </head>
 <body>
+  <header>
+      <?php require_once("nav.inc.php"); ?>
+  </header>
 
 </body>
 </html>
