99c7da8d7be4511375a062691dab9ff4ba82e486
anouckdeben95 <r0456343@student.thomasmore.be>
2019-04-09 16:04:23 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,18 @@
 <?php
+require_once("classes/User.class.php");
 
+// check if all fields have input
+if(!empty($_POST)) {
+    $user = new User();
 
+    $user->setEmail($_POST['email']);
+    $user->setPassword($_POST['password']);
+    $user->setPasswordConfirmation($_POST['password_confirmation']);
+    
+    $result = $user->register();
+    var_dump($result);
+
+}
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -12,5 +24,36 @@
 </head>
 <body>
     
+    <form action="" method="post">
+        <h2 form__title>Sign up for an account</h2>
+
+            <div class="form__error hidden">
+                <p>
+                    Some error here.
+                </p>
+            </div>
+
+            <div class="form__field">
+                <label for="email">Email</label>
+                <input type="text" id="email" name="email">
+            </div>
+            <div class="form__field">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+            </div>
+
+            <div class="form__field">
+                <label for="password_confirmation">Confirm your password</label>
+                <input type="password" id="password_confirmation" name="password_confirmation">
+            </div>
+
+            <div class="form__field">
+                <input type="submit" value="Sign me up!" class="btn btn--primary">  
+            </div>
+    </form>
+
+
+
+
 </body>
-</html>
+</html></html>
