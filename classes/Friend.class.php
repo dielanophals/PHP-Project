3e8c84d7cebd1d194f6c2d327e8761b4706e8d28
haege <haegeman.irene@hotmail.com>
2019-04-22 20:55:53 +0200

    Create Friend.class.php

---
+++ classes/Friend.class.php
@@ -0,0 +1,10 @@
+<?php
+Abstract class Friend{
+    static function getListOfFriends(){
+        //get a list of all the friends of user x
+    }
+
+    static function addFriend(){
+        //user adds new friend
+    }
+}
