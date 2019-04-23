d125271711bde6c536b8c397c3e4c6bbbc4981fb
haege <haegeman.irene@hotmail.com>
2019-04-23 20:45:33 +0200

    Merge functions of ShowUserInfo.class.php

--- classes/Show.class.php
+++ classes/Show.class.php
@@ -1,4 +1,11 @@
 <?php
     Abstract class Show{
-        
+        // get username & description
+        public function getUserInfo($userID) {
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("SELECT * FROM users WHERE id = '$userID'");
+            $statement->execute();
+            $information = $statement->fetchAll();
+            return $information;
+        }
     }
