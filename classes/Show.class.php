a19c68b9c8ed661b133cba3ec6295d4e6a804bc9
haege <haegeman.irene@hotmail.com>
2019-04-23 20:46:22 +0200

    Merge functions of ShowUserPosts.class.php

--- classes/Show.class.php
+++ classes/Show.class.php
@@ -8,4 +8,12 @@
             $information = $statement->fetchAll();
             return $information;
         }
+
+        public function getUserPosts($userID){
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("SELECT * FROM posts WHERE user_id = '$userID'");
+            $statement->execute();
+            $posts = $statement->fetchAll();
+            return $posts;
+        }
     }
