ee675336661a4dccd533916d5ae9b981079cc229
haege <haegeman.irene@hotmail.com>
2019-04-23 22:18:50 +0200

    Add posts to home screen

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
     if($s === false){
         header("Location: login.php");
     }
+
+    $id = $_SESSION['userID'];
+    $list = Friend::getListOfFriendsIds($id);
+    
+    $limit = 20;
+    if(empty($list)){
+        $err_posts = false;
+    }
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,11 +20,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel = "stylesheet" type = "text/css" href = "css/reset.css"/>
     <link rel = "stylesheet" type = "text/css" href = "css/style.css"/>
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
     <title>InstaPet - Feed</title>
 </head>
 <body>
     <header>
         <?php require_once("nav.inc.php"); ?>
     </header>
+    <main class="grid">
+        <?php if(isset($err_posts)): ?>
+            <p>No posts to display of your friends.</p>
+        <?php endif; ?>
+        <!--in id the id of the post is displayed, in case of detail page that needs to be shown.-->
+        <?php foreach($list as $key => $value): ?>
+            <?php
+                $posts = Show::getFriendsPosts($value, $limit);
+                foreach($posts as $k => $v):
+            ?>
+                <img src="<?php echo $v["image"]; ?>">
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </main>
 </body>
 </html>
