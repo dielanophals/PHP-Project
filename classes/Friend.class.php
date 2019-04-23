384dbbabf32c5cbf2556e9d14fa013c7cf58b20a
haege <haegeman.irene@hotmail.com>
2019-04-23 15:45:51 +0200

    Add static function getListOfFriendsIds()

--- classes/Friend.class.php
+++ classes/Friend.class.php
@@ -1,7 +1,28 @@
 <?php
 Abstract class Friend{
-    static function getListOfFriends(){
-        //get a list of all the friends of user x
+    static function getListOfFriendsIds(){
+        $p_iUserid = 1;
+        try{
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("SELECT user1_id, user2_id FROM `friends` INNER JOIN users ON friends.user1_id = users.id WHERE friends.user1_id = :user1_id OR friends.user2_id = :user2_id");
+            $statement->bindParam(":user1_id", $p_iUserid);
+            $statement->bindParam(":user2_id", $p_iUserid);
+            $statement->execute();
+            $list = $statement->fetch(PDO::FETCH_ASSOC);
+
+            $listOfIds = array();
+
+            foreach($list as $key => $value){
+                if($value !== $p_iUserid){
+                    array_push($listOfIds, "{$value}");
+                }
+            }
+
+            return $listOfIds;
+        }
+        catch(Throwable $t){
+            return false;
+        }
     }
 
     static function addFriend(){
