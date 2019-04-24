05939bfd9fb6dc8e79b1b273134427a99e8533c0
haege <haegeman.irene@hotmail.com>
2019-04-25 00:45:55 +0200

    Add ajax to load more btn

---
+++ js/index.js
@@ -0,0 +1,27 @@
+$(document).ready(() => {
+    $(".loadmore").click(function(e) {
+        let lastPostId = $(".grid div:last").attr("id");
+        $(".loadmore").html("Loading...");
+        
+        $.ajax({
+            method: "POST",
+            url: "ajax/friendsPosts.php",
+            data: {"lastId": lastPostId}, datatype: 'json'
+        })
+        .done(function(res) {
+            if(res.status = "success"){
+                let list = JSON.parse(res);
+
+                for(let i = 0; i <= 20; i++){
+                    if(typeof list.message[i] !== "undefined"){
+                        console.log(list.message[i][0]);
+                        var div = `<div id=${list.message[i][0]}><img src="${list.message[i][1]}"></div>`;
+                        $(".grid").append(div);
+                    }
+                }
+            }
+        });
+
+        e.preventDefault();
+    });
+});
