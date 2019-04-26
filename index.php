6adb119a69988794fdf71fc0ac466116332f88b3
haege <haegeman.irene@hotmail.com>
2019-04-26 19:47:59 +0200

    Add script like.js

--- index.php
+++ index.php
@@ -114,4 +114,5 @@
 </body>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="js/index.js"></script>
+    <script src="js/like.js"></script>
 </html>
