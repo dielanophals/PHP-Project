32a613d6212b8b32c2dfc32f1977cb546105070c
haege <haegeman.irene@hotmail.com>
2019-04-30 22:20:47 +0200

    Move functions to correct class User

--- classes/User.class.php
+++ classes/User.class.php
@@ -201,4 +201,20 @@
                 return false;
             }
         }
+
+        public static function getUserInfo($userID) {
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("SELECT * FROM users WHERE id = '$userID'");
+            $statement->execute();
+            $information = $statement->fetchAll();
+            return $information;
+        }
+
+        public static function getUserPosts($userID){
+            $conn = Db::getInstance();
+            $statement = $conn->prepare("SELECT * FROM posts WHERE user_id = '$userID'");
+            $statement->execute();
+            $posts = $statement->fetchAll();
+            return $posts;
+        }
     }
