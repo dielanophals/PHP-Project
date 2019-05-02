2fa85c5ed4221e140be58c35777570c82ac21799
haege <haegeman.irene@hotmail.com>
2019-05-02 16:02:48 +0200

    Add username to loaded posts popup

--- js/index.js
+++ js/index.js
@@ -11,13 +11,13 @@ $(document).ready(() => {
         .done(function(res) {
             if(res.status = "success"){
                 let list = JSON.parse(res);
-
                 for(let i = 0; i <= 20; i++){
                     if(typeof list.message[i] !== "undefined"){
                         var div =
                             `<a class="post__full" href="?image=${list.message[i][0]}?>">
                                 <div id=${list.message[i][0]} class="post">
-                                    <img class="post__img" src="${list.message[i][1]}">
+                                    <img class="post__img" src="${list.message[i][2]}">
+                                    <p class="post__name">${list.message[i][1]}</p>
                                 </div>
                             </a>`;
                         $(".grid").append(div);
