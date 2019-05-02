d9a5dd4499358ef433a21da26958aa3c592450a3
haege <haegeman.irene@hotmail.com>
2019-05-02 15:09:11 +0200

    Make code of post readable

--- js/index.js
+++ js/index.js
@@ -14,7 +14,12 @@ $(document).ready(() => {
 
                 for(let i = 0; i <= 20; i++){
                     if(typeof list.message[i] !== "undefined"){
-                        var div = `<a class="post__full" href="?image=${list.message[i][0]}?>"><div id=${list.message[i][0]} class="post"><img class="post__img" src="${list.message[i][1]}"></div></a>`;
+                        var div =
+                            `<a class="post__full" href="?image=${list.message[i][0]}?>">
+                                <div id=${list.message[i][0]} class="post">
+                                    <img class="post__img" src="${list.message[i][1]}">
+                                </div>
+                            </a>`;
                         $(".grid").append(div);
                     }
                 }
