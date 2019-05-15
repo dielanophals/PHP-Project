0a638dde5951a520cbdc98325e8e139188849eea
anouckdeben95 <r0456343@student.thomasmore.be>
2019-05-15 19:52:57 +0200

    Update comment.js

--- js/comment.js
+++ js/comment.js
@@ -1,26 +1,46 @@
-$(document).ready(() => {
-    $("#btnSubmit").on("click", function(e){
-        var text = $("#comment").val();
-        console.log("test");
-        
-        $.ajax({
-            method: "POST",
-            url: "ajax/save_comment.php",
-            data: { text: text },
-            dataType: 'json'
-        })
-        .done(function( response ) {
-            if( response.status == "success" ){
-                // = "<li>" + text + "</li>"
-                var li = "<li style='display:none;'>" + text + "</li>";
-                $("#listupdates").append(li);
-                $("#comment").val("").focus();
-                //animatie
-                $("#listupdates li").last().slideDown();
-            }
-        });
-        
-        
-        e.preventDefault();
+$(document).ready(function(){
+ 
+    $user_id = User::getUserInfo($_SESSION['userID']);
+
+    $('#comment_form').on('submit', function(event){
+     event.preventDefault();
+     var form_data = $(this).serialize();
+     $.ajax({
+      url:"ajax/saveComment.php",
+      method:"POST",
+      data:form_data,
+      dataType:"JSON",
+      success:function(data)
+      {
+       if(data.error != '')
+       {
+        $('#comment_form')[0].reset();
+        $('#comment_message').html(data.error);
+        $('#comment_id').val('0');
+        load_comment();
+       }
+      }
+     })
     });
-});
+   
+    load_comment();
+   
+    function load_comment()
+    {
+     $.ajax({
+      url:"ajax/getComments.php",
+      method:"POST",
+      success:function(data)
+      {
+       $('#display_comment').html(data);
+      }
+     })
+    }
+   
+    $(document).on('click', '.reply', function(){
+     var comment_id = $(this).attr("id");
+     $('#comment_id').val(comment_id);
+     //$('#comment_name').focus();
+    });
+    
+   });
