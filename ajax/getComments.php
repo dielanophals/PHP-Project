2a652b3b025e1ebdea523719da3dfb3afc361d8a
anouckdeben95 <r0456343@student.thomasmore.be>
2019-05-15 19:52:03 +0200

    Create getComments.php
    de bestaande comments en replies ophalen

---
+++ ajax/getComments.php
@@ -0,0 +1,72 @@
+<?php
+require_once("../bootstrap.php");
+Session::check();
+
+$connect = Db::getInstance();
+
+$query = "
+SELECT comments.id, parent_comment_id, comments.user_id, users.firstname, users.lastname, comments.comment, comments.date FROM comments, users 
+WHERE comments.user_id = users.id
+AND parent_comment_id = '0' 
+ORDER BY comments.id DESC
+";
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+$result = $statement->fetchAll();
+$output = '';
+foreach($result as $row)
+{
+ $output .= '
+ <div class="panel panel-default">
+  <div class="panel-heading">By <b>'.$row["firstname"].'</b> on <i>'.$row["date"].'</i></div>
+  <div class="panel-body">'.$row["comment"].'</div>
+  <div class="panel-footer" align="right"><button type="button" class="btn btn-default reply" id="'.$row["id"].'">Reply</button></div>
+ </div>
+ ';
+ $output .= get_reply_comment($connect, $row["id"]);
+}
+
+echo $output;
+
+function get_reply_comment($connect, $parent_id = 0, $marginleft = 0)
+{
+ $query = "
+ SELECT comments.id, parent_comment_id, comments.user_id, users.firstname, users.lastname, comments.comment, comments.date 
+ FROM comments, users 
+ WHERE comments.user_id = users.id
+ AND parent_comment_id = '".$parent_id."'
+ ";
+ $output = '';
+ $statement = $connect->prepare($query);
+ $statement->execute();
+ $result = $statement->fetchAll();
+ $count = $statement->rowCount();
+ if($parent_id == 0)
+ {
+  $marginleft = 0;
+ }
+ else
+ {
+  $marginleft = $marginleft + 48;
+ }
+ if($count > 0)
+ {
+  foreach($result as $row)
+  {
+   $output .= '
+   <div class="panel panel-default" style="margin-left:'.$marginleft.'px">
+    <div class="panel-heading">By <b>'.$row["firstname"].'</b> on <i>'.$row["date"].'</i></div>
+    <div class="panel-body">'.$row["comment"].'</div>
+    <div class="panel-footer" align="right"><button type="button" class="btn btn-default reply" id="'.$row["id"].'">Reply</button></div>
+   </div>
+   ';
+   $output .= get_reply_comment($connect, $row["id"], $marginleft);
+  }
+ }
+ return $output;
+}
+
+?>
