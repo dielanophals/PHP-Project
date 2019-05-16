ff35ef262e1e47ca805cb6e1f03ae6dc6495af9b
anouckdeben95 <r0456343@student.thomasmore.be>
2019-05-16 17:00:52 +0200

    Create saveComment.php
    putting input of textarea in database with user_id

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="css/vendor/cssgram.min.css">
+    <link rel="stylesheet" href="css/comment.css">
     <title>InstaPet - Feed</title>
     <style>
 
